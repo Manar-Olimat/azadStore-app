b84f696af5092d04a96298d0705aacf7fa3ee922
Manar-Olimat <manar.olimat07@gmail.com>
2022-10-28 19:06:01 +0200

    init files

---
+++ resources/js/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        <h1>Laravel Vite react js</h1>
+        <button> back</button>
+    </div>
+  )
+}
+
+export default App
