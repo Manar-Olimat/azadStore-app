70555ab125eae7394cb32a67ba68e29a605f5b3c
Manar-Olimat <manar.olimat07@gmail.com>
2022-11-11 23:23:51 +0200

    edit design

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,37 @@
+<!-- Basic Page Needs
+  ================================================== -->
+  <meta charset="utf-8">
+  <title>Aviato | E-commerce template</title>
+
+  <!-- Mobile Specific Metas
+  ================================================== -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="Construction Html5 Template">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+  <meta name="author" content="Themefisher">
+  <meta name="generator" content="Themefisher Constra HTML Template v1.0">
+  
+  <!-- Favicon -->
+  <link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" />
+  
+  <!-- Themefisher Icon font -->
+  <!-- <link rel="stylesheet" href="plugins/themefisher-font/style.css"> -->
+  <!-- bootstrap.min css -->
+  <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+  
+  <!-- Animate css -->
+  <!-- <link rel="stylesheet" href="plugins/animate/animate.css"> -->
+  <!-- Slick Carousel -->
+  <!-- <link rel="stylesheet" href="plugins/slick/slick.css"> -->
+  <!-- <link rel="stylesheet" href="plugins/slick/slick-theme.css"> -->
+
+    {{-- font awesome --}}
+<script src="https://kit.fontawesome.com/8b42dcad4f.js" crossorigin="anonymous"></script>
+
+  {{-- Google Fonts --}}
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Amiri&family=Cinzel:wght@500&family=El+Messiri:wght@600&family=Ibarra+Real+Nova:ital,wght@1,500;1,700&family=Kalam:wght@700&family=Quicksand&family=Roboto+Slab:wght@200&family=Scheherazade+New&family=Tajawal:wght@300&display=swap" rel="stylesheet">
+
+  <!-- Main Stylesheet -->
+  <link rel="stylesheet" href="css/style.css">
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,154 @@
+<!-- Start Top Header Bar -->
+<section class="top-header">
+	<div class="container">
+		<div class="row">
+			
+			<div class="col-md-4 col-xs-12 col-sm-4">
+				<!-- Site Logo -->
+				<div class="logo text-center">
+					<a href="index.html">
+						<!-- replace logo here -->
+						<img src="{{ asset('images/logo.png') }}" alt="Logo" width="100" height="90"
+							class="d-inline-block align-text-top logo">
+						<svg width="135px" height="29px" viewBox="0 0 155 29" version="1.1" xmlns="http://www.w3.org/2000/svg"
+							xmlns:xlink="http://www.w3.org/1999/xlink">
+							
+							<g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" font-size="40"
+								font-family="'El Messiri', sans-serif" font-weight="bold">
+								<g id="Group" transform="translate(-108.000000, -297.000000)" fill="#000000">
+									
+									<text id="AZAD">
+										<tspan x="108.94" y="325">AZAD</tspan>
+									</text>
+								</g>
+							</g>
+						</svg>
+					</a>
+				</div>
+			</div>
+			<div class="col-md-4 col-xs-12 col-sm-4">
+				<!-- Cart -->
+				<ul class="top-menu text-right list-inline">
+                    
+						<!-- Search -->
+						<li class=" search searchbar searchbar-wrapper">
+						
+							<form role="search">
+								<div class="searchbar-center">
+									<div class="searchbar-input-spacer"></div>
+								<input
+								class="form-control  searchbar-input"
+								type="search"
+								 placeholder="Search Products ..." 
+								 aria-label="Search">
+								</div>
+								<button class="btn bg-white search-icon searchbar-icon" type="submit">
+									<i class="fa fa-search"></i>
+								  </button>
+								</form>
+						
+				</li><!-- / Search -->
+
+                <li class="dropdown cart-nav dropdown-slide">
+                    <a href="#!" 
+                    class="dropdown-toggle "
+                     data-toggle="dropdown" 
+                     data-hover="dropdown"><i
+                            class="fa fa-cart-shopping"></i></a>
+
+                            
+                
+                </li><!-- / Cart -->
+
+                
+					<li class="dropdown wishlist-nav dropdown-slide">
+						<a href="#!" 
+                      ><i
+								class="fa fa-heart"></i></a>
+
+                                
+					
+					</li><!-- / Cart -->
+
+				
+
+
+				</ul><!-- / .nav .navbar-nav .navbar-right -->
+			</div>
+		</div>
+	<div class="row">
+          <nav class="navbar navigation">
+		<div class="nav-menu">
+			<div class="navbar-header">
+				<h2 class="menu-title">Main Menu</h2>
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+					aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+
+			</div><!-- / .navbar-header -->
+
+			<!-- Navbar Links -->
+			<div id="navbar" class="navbar-collapse collapse text-center category-menu">
+				<ul class="nav navbar-nav">
+
+					<!-- Home -->
+					<li class="dropdown ">
+						<a href="index.html">Home</a>
+					</li><!-- / Home -->
+
+
+					<!-- Elements -->
+					<li class="dropdown dropdown-slide">
+						<a href="#!" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="350"
+							role="button" aria-haspopup="true" aria-expanded="false">Shop <span
+								class="tf-ion-ios-arrow-down"></span></a>
+						<div class="dropdown-menu">
+							<div class="row">
+
+								<!-- Basic -->
+								<div class="col-lg-6 col-md-6 mb-sm-3">
+									<ul>
+										<li class="dropdown-header">Pages</li>
+										<li role="separator" class="divider"></li>
+										<li><a href="shop.html">Shop</a></li>
+										<li><a href="checkout.html">Checkout</a></li>
+										<li><a href="cart.html">Cart</a></li>
+										<li><a href="pricing.html">Pricing</a></li>
+										<li><a href="confirmation.html">Confirmation</a></li>
+
+									</ul>
+								</div>
+
+								<!-- Layout -->
+								<div class="col-lg-6 col-md-6 mb-sm-3">
+									<ul>
+										<li class="dropdown-header">Layout</li>
+										<li role="separator" class="divider"></li>
+										<li><a href="product-single.html">Product Details</a></li>
+										<li><a href="shop-sidebar.html">Shop With Sidebar</a></li>
+
+									</ul>
+								</div>
+
+							</div><!-- / .row -->
+						</div><!-- / .dropdown-menu -->
+					</li><!-- / Elements -->
+
+
+					
+                    
+				</ul><!-- / .nav .navbar-nav -->
+
+			</div>
+			<!--/.navbar-collapse -->
+		</div><!-- / .container -->
+	</nav> 
+    </div>
+    </div>
+ 
+</section><!-- End Top Header Bar -->
+
